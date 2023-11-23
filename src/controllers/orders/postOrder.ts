b1555215b3ec9166b4644ec03406df7d407e71ab
nojood othman alghamdi <nojood.works@gmail.com>
2023-11-23 11:46:57 +0300

    user model is ready

--- src/controllers/orders/postOrder.ts
+++ src/controllers/orders/postOrder.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
-import User from '../../models/user'
+import User from '../../models/userSchema'
 import Order from '../../models/order'
 
 export const postOrder = async (req: Request, res: Response, next: NextFunction) => {
