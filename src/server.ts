a24eb7ebb3a0ed5953ebe6ac71a1dfe7aaacd5de
mrwaff4 <mreaff4@gmail.com>
2023-11-23 12:11:26 +0300

    test

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import { config } from 'dotenv'
 
-import usersRouter from './routers/users'
+//import usersRouter from './routers/users'
 import productsRouter from './routers/products'
 import ordersRouter from './routers/orders'
 import categoriesRouter from './routers/categoryRoutes'
@@ -18,7 +18,7 @@ app.use(myLogger)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use('/api/users', usersRouter)
+//app.use('/api/users', usersRouter)
 app.use('/api/orders', ordersRouter)
 app.use('/api/products', productsRouter)
 app.use('/api/categories',categoriesRouter)
