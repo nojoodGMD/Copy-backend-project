e748c08b7c14e0c64372abdae3086a7ee5449ff6
nojood othman alghamdi <nojood.works@gmail.com>
2023-11-26 01:35:18 +0300

    validation for user routers

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -2,6 +2,8 @@ import { Router } from 'express'
 
 import { deleteUser, getAllUsers, getUserById, registerUser, updateUser } from '../controllers/userController';
 import { uploadUser } from '../middlewares/uploadFile';
+import { validateCreateUser, validateUpdateUser } from '../validation/userVal';
+import { runValidation } from '../validation/runValidation';
 
 const router = Router();
 
@@ -19,10 +21,10 @@ router.get('/',getAllUsers);
 router.get('/:id',getUserById)
 
 //POST: /users/register -> register a new user
-router.post('/register',uploadUser.single("image"),registerUser);
+router.post('/register',validateCreateUser,runValidation,uploadUser.single("image"),registerUser);
 
 //PUT: /user/:id -> update the user data based on the id
-router.put('/:id',updateUser);
+router.put('/:id',validateUpdateUser,runValidation,updateUser);
 
 //DELETE: /users/:id -> delete the user based on the id
 router.delete('/:id',deleteUser)
