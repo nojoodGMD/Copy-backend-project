b090ea3bbfb4fd470a10d896b4b606b6b7617530
mashael aljaafari <mashaelaljafari1@gmail.com>
2023-11-27 09:48:45 +0300

    user-pagination

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,71 +1,83 @@
-import { Request } from 'express';
+import { Request } from 'express'
 
-import ApiError from "../errors/ApiError";
-import  User from '../models/userSchema';
+import ApiError from '../errors/ApiError'
+import User from '../models/userSchema'
 
 //GET-> get all users
-export const getAllUsersService = async () => {
-    const users = await User.find()
-    return users
+export const getAllUsersService = async (page: number, limit: number) => {
+  const count = await User.countDocuments()
+  if (count <= 0) {
+    throw new ApiError(404, 'There are no user yet to show.')
   }
+  const totalPage = Math.ceil(count / limit)
+  if (page > totalPage) {
+    page = totalPage
+  }
+  const skip = (page - 1) * limit
+  const users = await User.find().skip(skip).limit(limit)
+  return {
+    users,
+    totalPage,
+    currentPage: page,
+  }
+}
 
 //GET-> get user by id
 export const getSingleUserService = async (req: Request) => {
-    const id = req.params.id;
-    const users = await User.findOne({id:id});
-    if (!users) {
-        const error = new ApiError(404, `User with this id does not exist.`)
-        throw error
-    }
-    return users
+  const id = req.params.id
+  const users = await User.findOne({ id: id })
+  if (!users) {
+    const error = new ApiError(404, `User with this id does not exist.`)
+    throw error
+  }
+  return users
 }
 
 // Check if user already exists
-export const isUserExistService = async ( req : Request)=>{
-    const user = await User.exists({email : req.body.email})
-    if(user){
-      const error = new ApiError(409,'User with this email already exists.')
-      throw error;
-    }
+export const isUserExistService = async (req: Request) => {
+  const user = await User.exists({ email: req.body.email })
+  if (user) {
+    const error = new ApiError(409, 'User with this email already exists.')
+    throw error
+  }
 }
 
 //create a new user
-export const createUserService = async (req: Request)=>{
-    await isUserExistService(req);
-    const {name,email,password,phone} = req.body; 
-    const imagePath = req.file?.path;
+export const createUserService = async (req: Request) => {
+  await isUserExistService(req)
+  const { name, email, password, phone } = req.body
+  const imagePath = req.file?.path
 
-    const newUser = new User({
-        name:name,
-        email: email,
-        password: password,
-        phone: phone,
-        image: imagePath,
-    })
+  const newUser = new User({
+    name: name,
+    email: email,
+    password: password,
+    phone: phone,
+    image: imagePath,
+  })
 
-    await newUser.save();
-    return newUser;
+  await newUser.save()
+  return newUser
 }
 
 //delete user
-export const deleteUserSevice = async (req : Request)=>{
-    const id = req.params.id;
-    const user = await User.findOneAndDelete({id:id});
-    if(!user){
-      const error = new ApiError(404,"User with this id doesn't exist")
-      throw error; 
-    }
-
-} 
+export const deleteUserSevice = async (req: Request) => {
+  const id = req.params.id
+  const user = await User.findOneAndDelete({ id: id })
+  if (!user) {
+    const error = new ApiError(404, "User with this id doesn't exist")
+    throw error
+  }
+}
 
 //update user
-export const updateUserService = async (req: Request)=>{
-    const id = req.params.id; 
-    const newData = req.body;
-    const user = await User.findOneAndUpdate({id:id},newData,{new:true});
-    if(!user){
-      const error = new ApiError(404,"User with this id doesn't exist")
-      throw error;
-    }
-    return user;
+export const updateUserService = async (req: Request) => {
+  const id = req.params.id
+  const newData = req.body
+  const user = await User.findOneAndUpdate({ id: id }, newData, { new: true })
+  if (!user) {
+    const error = new ApiError(404, "User with this id doesn't exist")
+    throw error
+  }
+  return user
 }
