d0dea4571f405a88ca6558973fabba5217ee26f8
mrwaff4 <mreaff4@gmail.com>
2023-11-27 14:48:50 +0300

    create filter product by price

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,17 +1,24 @@
-
 import slugify from 'slugify'
 import { Request, Response, NextFunction } from 'express'
 
 import { Product } from '../models/productSchema'
-import { findProductBySlug, newProduct, productService, removeProductBySlug, updateProductServices } from '../services/productServices'
+import {
+  findProductBySlug,
+  newProduct,
+  productService,
+  removeProductBySlug,
+  updateProductServices,
+} from '../services/productServices'
 
-//return all productes
+//return all products
 export const getAllProducts = async (req: Request, res: Response, next: NextFunction) => {
   try {
     let page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || 3
+    const minPrice = Number(req.query.minPrice) || 0
+    const maxPrice = Number(req.query.maxPrice) || 200000
 
-    const result = await productService(page, limit)
+    const result = await productService(page, limit, minPrice, maxPrice)
 
     res.send({
       message: 'Get all products',
@@ -27,67 +34,52 @@ export const getAllProducts = async (req: Request, res: Response, next: NextFunc
 }
 
 //return a single product
-export const getProductBySlug = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const getProductBySlug = async (req: Request, res: Response, next: NextFunction) => {
   try {
     // const slug = req.params.slug;
-    const product = await findProductBySlug (req.params.slug);
+    const product = await findProductBySlug(req.params.slug)
     if (!product) {
-      throw new Error("Product not found with this slug!");
+      throw new Error('Product not found with this slug!')
     }
-    res.send({ message: "Get a single product", payload: product });
+    res.send({ message: 'Get a single product', payload: product })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
 //Crete a product
 export const createProduct = async (req: Request, res: Response, next: NextFunction) => {
   try {
-      const { name, price, quantity, description, sold, shipping } = req.body;
+    const { name, price, quantity, description, sold, shipping } = req.body
 
+    const newItem = await newProduct(name, price, quantity, description, sold, shipping)
 
-     const newItem =  await newProduct(name, price, quantity, description, sold, shipping);
-
-      res.status(201).send({ message: 'Product is created' });
+    res.status(201).send({ message: 'Product is created' })
   } catch (error) {
-      next(error);
+    next(error)
   }
-};
+}
 
 //Delete a product
-export const deleteProductBySlug = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const deleteProductBySlug = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const product = await removeProductBySlug(req.params.slug);
-    res.send({ message: "Deleted a single product", payload: product });
+    const product = await removeProductBySlug(req.params.slug)
+    res.send({ message: 'Deleted a single product', payload: product })
   } catch (error) {
-    next(error);
+    next(error)
   }
-};
+}
 
 // Update a product
-export const updateProductBySlug = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-
-    try { 
-      const { slug } = req.params; 
-      const product= await updateProductServices(req,slug) 
-      res.json({ 
-        message: " product is updated", 
-        payload: product, 
-      }); 
-    } catch (error) { 
-      next(error); 
-    } 
-  };
-
+export const updateProductBySlug = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { slug } = req.params
+    const product = await updateProductServices(req, slug)
+    res.json({
+      message: ' product is updated',
+      payload: product,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
--- src/services/productServices.ts
+++ src/services/productServices.ts
@@ -1,110 +1,111 @@
-import slugify from "slugify";
-import { Request } from 'express';
+import slugify from 'slugify'
+import { Request } from 'express'
 
-import { IProduct, Product } from "../models/productSchema";
-import { createHttpError } from "../errors/createError";
-import ApiError from "../errors/ApiError";
-import apiErrorHandler from "../middlewares/errorHandler";
-
-
+import { IProduct, Product } from '../models/productSchema'
+import { createHttpError } from '../errors/createError'
+import ApiError from '../errors/ApiError'
+import apiErrorHandler from '../middlewares/errorHandler'
 
 //GET->get all the product services
-export const productService = async (page: number, limit: number) => {
-    const count = await Product.countDocuments();
-    if(count <= 0){
-        throw new ApiError(404,'There are no products yet to show, please add products.')
-    }
-
-    const totalPage = Math.ceil(count / limit);
-    if (page > totalPage) {
-        page = totalPage;
-    }
-    const skip = (page - 1) * limit;
-
-    const products = await Product.find().sort({name : 1}).skip(skip).limit(limit);
-
-    return {
-        products,
-        totalPage,
-        currentPage: page,
-    };
-};
-
-//get single prooduct
-export const findProductBySlug = async (slug: string): Promise<IProduct> => {
-
-    const product = await Product.findOne({ slug: slug });
-    if (!product) {
-      throw new Error("Product not found with this slug!");
-    }
-    return product;
-  };
-  
-  //delete a single product
-export const removeProductBySlug = async (slug: string): Promise<IProduct> => {  
-    const product = await Product.findOneAndDelete({ 
-     slug: slug
-     });
-     if (!product) {
-       throw new Error("Product not found with this slug!");
-     }
-     return product;
-   };
-
-   //create a product
-export const newProduct = async (
-    name: string,
-    price: number,
-    quantity: number,
-    description: string,
-    sold: boolean,
-    shipping: string
+export const productService = async (
+  page: number,
+  limit: number,
+  minPrice: number,
+  maxPrice: number
 ) => {
-    // Check if a product with the same name already exists
-    const productExist = await Product.exists({ name });
+  const count = await Product.countDocuments()
+  if (count <= 0) {
+    throw new ApiError(404, 'There are no products yet to show, please add products.')
+  }
 
-    if (productExist) {
-        throw createHttpError(409, 'Product already exists with this name');
-    }
+  const totalPage = Math.ceil(count / limit)
+  if (page > totalPage) {
+    page = totalPage
+  }
+  const skip = (page - 1) * limit
+  const filterProductsByPrice = {
+    $and: [{ price: { $gt: minPrice } }, { price: { $lt: maxPrice } }],
+  }
+  const products = await Product.find(filterProductsByPrice).sort({ name: 1 }).skip(skip).limit(limit)
 
-    const newProduct = new Product({
-        name,
-        slug: slugify(name),
-        price,
-        quantity,
-        description,
-        sold,
-        shipping,
-    });
+  return {
+    products,
+    totalPage,
+    currentPage: page,
+  }
+}
 
-    await newProduct.save();
-};
+//get single products
+export const findProductBySlug = async (slug: string): Promise<IProduct> => {
+  const product = await Product.findOne({ slug: slug })
+  if (!product) {
+    throw new Error('Product not found with this slug!')
+  }
+  return product
+}
+
+//delete a single product
+export const removeProductBySlug = async (slug: string): Promise<IProduct> => {
+  const product = await Product.findOneAndDelete({
+    slug: slug,
+  })
+  if (!product) {
+    throw new Error('Product not found with this slug!')
+  }
+  return product
+}
+
+//create a product
+export const newProduct = async (
+  name: string,
+  price: number,
+  quantity: number,
+  description: string,
+  sold: boolean,
+  shipping: string
+) => {
+  // Check if a product with the same name already exists
+  const productExist = await Product.exists({ name })
+
+  if (productExist) {
+    throw createHttpError(409, 'Product already exists with this name')
+  }
+
+  const newProduct = new Product({
+    name,
+    slug: slugify(name),
+    price,
+    quantity,
+    description,
+    sold,
+    shipping,
+  })
+
+  await newProduct.save()
+}
 
 //update a product
-export const updateProductServices=async(req:Request, slug:string): Promise <IProduct>=>{ 
-    if (req.body.slug) { 
-      //update the slug value 
-      req.body.slug = slugify(req.body.name); 
-    } 
-     
-    // Always update the slug based on the name
-    const updatedSlug = slugify(req.body.name);
-  
-    const updateData = {
-      ...req.body,
-      slug: updatedSlug,
-    };
-    
-    //in this function i base 2 things 1- is id 2- is udated data and i will get this data from req.body 
-    const product = await Product.findOneAndUpdate({ slug }, updateData, { 
-      new: true, 
-    }); 
-    if (!product) { 
-      const error = createHttpError( 
-        404, 
-        `Product is not found with this slug: ${slug}`
-      ); 
-      throw error; 
-    } 
-    return product 
+export const updateProductServices = async (req: Request, slug: string): Promise<IProduct> => {
+  if (req.body.slug) {
+    //update the slug value
+    req.body.slug = slugify(req.body.name)
   }
+
+  // Always update the slug based on the name
+  const updatedSlug = slugify(req.body.name)
+
+  const updateData = {
+    ...req.body,
+    slug: updatedSlug,
+  }
+
+  //in this function i base 2 things 1- is id 2- is udated data and i will get this data from req.body
+  const product = await Product.findOneAndUpdate({ slug }, updateData, {
+    new: true,
+  })
+  if (!product) {
+    const error = createHttpError(404, `Product is not found with this slug: ${slug}`)
+    throw error
+  }
+  return product
+}
