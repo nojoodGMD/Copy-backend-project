3fa505758290fc6aaf4860c694149f813d828850
Nojood Othman <nojood.works@gmail.com>
2023-11-28 16:35:31 +0300

    Remove console.log

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -3,8 +3,6 @@ import { NextFunction, Request, Response } from 'express'
 import ApiError from '../errors/ApiError'
 
 const apiErrorHandler = (err: typeof ApiError, req: Request, res: Response, next: NextFunction) => {
-  console.log('inside api error handler: ', err) 
-  console.log('type of error', typeof err) 
   if (err instanceof ApiError) {
     res.status(err.code).json({ msg: err.message })
     return
